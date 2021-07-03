9c7db8724d3e4c6340dc76b52c7c684605643061
JordyToke <jordyn.toke@gmail.com>
2021-07-03 17:15:31 +1000

    created week5lab text_server.py

---
+++ text_server.py
@@ -0,0 +1,64 @@
+import string
+from flask import Flask, request
+from pyhtml import html, head, title, body, title, p, table, tr, td, form, label, input_, strong
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    code = html(
+        head(
+            title("string analysis")
+        ),
+        body(
+            p("Please enter your text and submit"),
+            form(action="analysis")
+            (
+                label("String: "),
+                input_(type="text", name="string"),
+                input_(type="Submit", value="analyse")
+            )
+        )
+    )
+    return str(code)
+
+@app.route('/analysis', methods=["POST"])
+def analysis():
+
+    #print request.form
+    print(f"{request.form=}")
+    text = request.form['string']
+
+    total_length = len(text)
+    char_length = 0
+    for char in text:
+        if char not in string.punctuation:
+            char_length += 1
+    
+    words = len(text.split())
+
+    code = html(
+        head(
+            title("string analysis")
+        ),
+        body(
+            p("Returned string analysis"),
+            table(style="width:400px")(
+                tr(
+                    td(strong("Total length:")),
+                    td(strong("Length minus punctuation:")),
+                    td(strong("Words:"))
+                ),
+                tr(
+                    td(f"{total_length}"),
+                    td(f"{char_length}"),
+                    td(f"{words}")
+                )
+            )
+        )
+    )
+
+    return str(code)
+
+if __name__ == "__main__":
+    app.run(debug=True)
